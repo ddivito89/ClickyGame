aed2267bf09619712623f7852b2e3c933e13abfc
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-05-03 22:01:15 -0500

    nav

--- clickygame/src/components/Nav.js
+++ clickygame/src/components/Nav.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Nav = props => (
   <nav className=" nav-fill w-100 navbar navbar-expand-lg navbar-light bg-dark">
-    <a className="navbar-brand" href="/">CLICKY GAME</a>
     <ul className="navbar-nav navbar-collapse">
       <li className="col-sm-12 col-md-4 col-lg-3 nav-item">{props.alert}</li>
       <li className="col-sm-12 col-md-4 col-lg-3 nav-item">Score:{props.score}</li>
